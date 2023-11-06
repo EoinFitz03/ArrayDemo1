52ba7ba3fcf6e5b2d3ee46dd4ab53b55c83d5a7f
EoinFirz03 <G00418325@atu.ie>
2023-11-06 10:38:19 +0000

    Feature1 adding in ArrayList Demo

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,33 +1,48 @@
 package ie.atu;
 
+import java.util.Scanner;
+
+import java.util.ArrayList;
+
+
+
 public class Main {
     public static void main(String[] args) {
+        int i=0;
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(100);
+        numbers.add(200);
+        numbers.add(300);
+        numbers.add(400);
+        numbers.add(500);
+        numbers.add(600);
+        numbers.add(700);
+        numbers.add(800);
+        numbers.add(900);
+        numbers.add(1000);
+
+        int secondNumber = numbers.get(1);
 
 
-        int[] anArray;
-        anArray = new int[10];
-
-        anArray[0] = 100;
-        anArray[1] = 200;
-        anArray[2] = 300;
-        anArray[3] = 400;
-        anArray[4] = 500;
-        anArray[5] = 600;
-        anArray[6] = 700;
-        anArray[7] = 800;
-        anArray[8] = 900;
-        anArray[9] = 1000;
 
 
-        System.out.println("Element at index 0 " + anArray[0]);
-        System.out.println("Element at index 1 " + anArray[1]);
-        System.out.println("Element at index 2 " + anArray[2]);
-        System.out.println("Element at index 3 " + anArray[3]);
-        System.out.println("Element at index 4 " + anArray[4]);
-        System.out.println("Element at index 5 " + anArray[5]);
-        System.out.println("Element at index 6 " + anArray[6]);
-        System.out.println("Element at index 7 " + anArray[7]);
-        System.out.println("Element at index 8 " + anArray[8]);
-        System.out.println("Element at index 9 " + anArray[9]);
+        for(int number : numbers){
+
+            System.out.println("Element at index "+i+ ":"+ number);
+            i++;
+        }
+        i=0;
+
+        System.out.println("Please Enter a new number ");
+        Scanner scanner = new Scanner(System.in);
+        numbers.add (scanner.nextInt());
+
+        for(int number : numbers){
+
+            System.out.println("Element at index "+i+ ":"+ number);
+            i++;
+        }
+
     }
 }
